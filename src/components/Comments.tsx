fbb4fac0d8b2ee3b8f62a63a463557d72cbe9c3f
David Silva <contatodavidmaia@gmail.com>
2023-02-19 15:52:55 -0300

    :sparkles: feat: add comment history date and adjust comment design

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -23,35 +23,71 @@ export const Comments: FC<CommentsProps> = ({ id }) => {
 
       const { data } = await instance.get(MOVIE_REVIEWS_URL)
 
+      console.log(data.results)
+
       return data.results
     }
   )
 
+  function getCommentDate(date: string) {
+    const now = new Date()
+
+    let currentYear = now.getFullYear()
+    const commentYear = Number(date.slice(0, -20))
+
+    let currentMonth = now.getMonth() + 1
+    const commentMonth = Number(date.slice(0, -14).slice(-5).slice(0, -3))
+
+    let currentDay = now.getDate()
+    const commentDay = Number(date.slice(0, -14).slice(-2))
+
+    if (currentYear > commentYear) {
+      return currentMonth > commentMonth && currentYear - commentYear > 1
+        ? `${currentYear - commentYear}a atrás`
+        : `${currentMonth - commentMonth + 12}m atrás`
+    }
+
+    if (currentMonth > commentMonth) {
+      return `${currentMonth - commentMonth}m atrás`
+    }
+
+    if (currentDay > commentDay) {
+      return `${currentDay - commentDay}d atrás`
+    }
+  }
+
   return (
-    <section className="flex w-full flex-col gap-3">
+    <section className="mb-6 flex w-full flex-col gap-3">
       <h3>Comentários</h3>
       {comments && comments.length > 0 ? (
         comments?.map(comment => (
           <article
-            className="flex items-start gap-8 rounded-md bg-cadet bg-opacity-10 py-3 px-5 shadow-lg"
+            className="flex flex-col items-start gap-3 rounded-md bg-secondary-700 py-3 px-5 shadow-lg"
             key={comment.id}
           >
-            {comment.author_details.avatar_path && (
-              <img
-                className="w-12 rounded-full"
-                src={
-                  comment.author_details.avatar_path
-                    ? comment.author_details.avatar_path.startsWith('/http')
-                      ? comment.author_details.avatar_path.substring(1)
-                      : `https://www.themoviedb.org/t/p/w300_and_h300_face${comment.author_details.avatar_path}`
-                    : `https://ui-avatars.com/api/?name=${comment.author}`
-                }
-                alt={'Avatar de ' + comment.author}
-              />
-            )}
+            <header className="flex items-center gap-3">
+              {comment.author_details && (
+                <img
+                  className="w-8 rounded-full"
+                  src={
+                    comment.author_details.avatar_path !== null
+                      ? comment.author_details.avatar_path.startsWith('/http')
+                        ? comment.author_details.avatar_path.substring(1)
+                        : `https://www.themoviedb.org/t/p/w300_and_h300_face${comment.author_details.avatar_path}`
+                      : `https://ui-avatars.com/api/?name=${comment.author}`
+                  }
+                  alt={'Avatar de ' + comment.author}
+                />
+              )}
+              <h4 className="flex items-center gap-2 text-sm font-medium">
+                {comment.author}{' '}
+                <span className="flex items-center gap-2 text-xs text-secondary-300">
+                  {getCommentDate(comment.created_at)}
+                </span>
+              </h4>
+            </header>
 
             <div>
-              <h4>{comment.author}</h4>
               <p className="text-xs text-secondary-50 opacity-80">
                 {comment.content}
               </p>
